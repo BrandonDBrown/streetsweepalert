493b09360c82e0fb9bf4fdca470e54ce8b5193dc
Brandon Brown <brandon.bbrandon@gmail.com>
2015-08-31 13:50:39 -0700

    added button focus

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
       auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    flash[:success] = 'Signed in'
+#    flash[:success] = 'Signed in'
     redirect_to user
 
                
