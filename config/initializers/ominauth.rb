95cb64ffe3207c32be334856110e9250f111c363
Brandon Brown <brandon.bbrandon@gmail.com>
2015-09-02 16:48:26 -0700

    splash screen stuff

--- config/initializers/ominauth.rb
+++ config/initializers/ominauth.rb
@@ -2,7 +2,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '845919965723-jte5403epcbbcagjqojk8325a2jchbnk.apps.googleusercontent.com', 'nBpNihevD0YVhkrL6j5tqEDc', {
   scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    access_type: 'offline', redirect_uri:'http://localhost:3000/auth/google_oauth2/callback' 
+    redirect_uri: 'https://still-castle-6480.herokuapp.com/auth/google_oauth2/callback'
     }
 end
 
@@ -10,4 +10,5 @@ end
 
 
 
-#redirect_uri: 'https://still-castle-6480.herokuapp.com/auth/google_oauth2/callback'
+
+#access_type: 'offline', redirect_uri:'http://localhost:3000/auth/google_oauth2/callback' 
