b2eeb00750cf11fe1e6841155a7fffc0135a786b
Brandon Brown <brandon.bbrandon@gmail.com>
2015-09-06 16:37:30 -0700

    added more streets working on icon and splash screen

--- config/initializers/ominauth.rb
+++ config/initializers/ominauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '845919965723-jte5403epcbbcagjqojk8325a2jchbnk.apps.googleusercontent.com', 'nBpNihevD0YVhkrL6j5tqEDc', {
   scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
       access_type: 'offline',  
-    redirect_uri: 'https://still-castle-6480.herokuapp.com/auth/google_oauth2/callback'
+    redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
     }
 end
 
@@ -16,5 +16,7 @@ end
 
 
 
-#redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
+
+
+#redirect_uri: 'https://still-castle-6480.herokuapp.com/auth/google_oauth2/callback'
 
