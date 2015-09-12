b89335a3d55ed51df71b619ea4d4f17bfb1262ce
Brandon Brown <brandon.bbrandon@gmail.com>
2015-09-11 18:46:33 -0700

    fixed a too many redirects error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
         if current_user
             redirect_to current_user
         else
-            redirect_to root_path #or whatever
+             #or whatever
         end
   end
 end
