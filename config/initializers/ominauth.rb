1f4e07a606a78fffe0d4bcf9f188493bd3b66f2c
Brandon Brown <brandon.bbrandon@gmail.com>
2015-09-12 14:03:51 -0700

    updating remember token method

--- config/initializers/ominauth.rb
+++ config/initializers/ominauth.rb
@@ -3,7 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '845919965723-jte5403epcbbcagjqojk8325a2jchbnk.apps.googleusercontent.com', 'nBpNihevD0YVhkrL6j5tqEDc', {
   scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',  
     redirect_uri: 'https://still-castle-6480.herokuapp.com/auth/google_oauth2/callback'
-
     }
 end
 
