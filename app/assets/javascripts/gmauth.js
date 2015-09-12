5863354a7afbf6cf1df13c91d9662dc9f2c82ec9
Brandon Brown <brandon.bbrandon@gmail.com>
2015-09-11 17:26:40 -0700

    tried moving the google authorization script to the application.html

---
+++ app/assets/javascripts/gmauth.js
@@ -0,0 +1,49 @@
+// Your Client ID can be retrieved from your project in the Google
+      // Developer Console, https://console.developers.google.com
+      var CLIENT_ID = '845919965723-jte5403epcbbcagjqojk8325a2jchbnk.apps.googleusercontent.com';
+      var SCOPES = ["https://www.googleapis.com/auth/calendar"];
+
+      
+      /**
+       * Check if current user has authorized this application.
+       */
+      function checkAuth() {
+        gapi.auth.authorize(
+          {
+            'client_id': CLIENT_ID,
+            'scope': SCOPES,
+            'immediate': true
+          }, handleAuthResult);
+      }
+
+      /**
+       * Handle response from authorization server.
+       *
+       * @param {Object} authResult Authorization result.
+       */
+      function handleAuthResult(authResult) {
+        
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+          // Hide auth UI, then load client library.
+//          authorizeDiv.style.display = 'none';
+//            loadCalendarApi();
+        } else {
+          // Show auth UI, allowing the user to initiate authorization by
+          // clicking authorize button.
+            alert('not logged in');
+//          authorizeDiv.style.display = 'inline';
+        }
+      }
+
+      /**
+       * Initiate auth flow in response to user clicking authorize button.
+       *
+       * @param {Event} event Button click event.
+       */
+//      function handleAuthClick(event) {
+//        gapi.auth.authorize(
+//          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+//          handleAuthResult);
+//        return false;
+//      }
