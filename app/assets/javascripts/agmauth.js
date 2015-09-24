0941cf4b29288ab9ce9e4b705f5b4e61fe76149f
Brandon Brown <brandon.bbrandon@gmail.com>
2015-09-24 10:49:33 -0700

    Minor updates

--- app/assets/javascripts/agmauth.js
+++ app/assets/javascripts/agmauth.js
@@ -2,7 +2,6 @@
       // Developer Console, https://console.developers.google.com
       var CLIENT_ID = '845919965723-jte5403epcbbcagjqojk8325a2jchbnk.apps.googleusercontent.com';
       var SCOPES = ["https://www.googleapis.com/auth/calendar"];
-
       
       /**
        * Check if current user has authorized this application.
@@ -16,39 +15,11 @@
           }, loadCalendarApi);
       }
 
+
+//SHOWS ADD TO HOMESCREEN BANNER AND REMOVES AFTER ADD TO HOMESCREEN
 $(document).ready(function() {
     if ('standalone' in navigator && !navigator.standalone && (/iphone|ipod|ipad/gi).test(navigator.platform) && (/Safari/i).test(navigator.appVersion)) {
     $('.banner').show()
 }
 });
-      /**
-       * Handle response from authorization server.
-       *
-       * @param {Object} authResult Authorization result.
-       */
-//      function handleAuthResult(authResult) {
-//        
-//        var authorizeDiv = document.getElementById('authorize-div');
-//        if (authResult && !authResult.error) {
-//          // Hide auth UI, then load client library.
-//          authorizeDiv.style.display = 'none';
-//            
-//        } else {
-//          // Show auth UI, allowing the user to initiate authorization by
-//          // clicking authorize button.
-////            alert('not logged in');
-////          authorizeDiv.style.display = 'inline';
-//        }
-//      }
-
-      /**
-       * Initiate auth flow in response to user clicking authorize button.
-       *
-       * @param {Event} event Button click event.
-       */
-//      function handleAuthClick(event) {
-//        gapi.auth.authorize(
-//          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-//          handleAuthResult);
-//        return false;
-//      }
+   
